1eec1f4be6c5eb220518d06e4502b71b1e90291d
Nikolai Saganenko <9952728@gmail.com>
2021-11-07 22:04:48 +0300

    Add ImagePicker Add applyProcessing function

--- InstaPhotoFilters/ViewController.swift
+++ InstaPhotoFilters/ViewController.swift
@@ -6,23 +6,58 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var currentImage = UIImage!
+    var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "InstaFilters"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPhoto))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {return}
+        dismiss(animated: true)
+        currentImage = image
+        
+        let filterImage = CIImage(image: currentImage)
+        currentFilter.setValue(filterImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    @objc func addPhoto() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func applyProcessing() {
+        currentFilter.setValue(slider.value, forKey: kCIInputIntensityKey)
+        
+        if let cgImage = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgImage)
+            imageView.image = processedImage
+        }
     }
 
     @IBAction func sliderChanged(_ sender: UISlider) {
+        applyProcessing()
     }
     
     @IBAction func changeFilter(_ sender: UIButton) {
+        
     }
     @IBAction func save(_ sender: UIButton) {
     }
