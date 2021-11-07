9df0b532ecd751350eb4e3d1cc786c206dc75987
Nikolai Saganenko <9952728@gmail.com>
2021-11-07 21:41:07 +0300

    Make UI

--- InstaPhotoFilters/ViewController.swift
+++ InstaPhotoFilters/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentImage = UIImage!
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderChanged(_ sender: UISlider) {
+    }
+    
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    @IBAction func save(_ sender: UIButton) {
+    }
 }
 
